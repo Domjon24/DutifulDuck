21b30556c9b0888b4baf0975d9c05837470464d6
Dominique <Domjon24@gmail.com>
2025-09-29 00:37:30 -0500

    FINALLY FINISHED 1ST ANIMATIONS. couldn't figure out sprite animation, so I converted sprite sheet to a gif as a placeholder for now. Styling needs to be updated. ready for V1 deployment

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -5,13 +5,14 @@ import {Box} from '../styles/box';
 import {Flex} from '../styles/flex';
 import { useState } from "react";
 import axios from "axios";
+import { keyframes } from '@emotion/react';
 
 export const Hero = () => {
+  const [response, setResponse] = useState<string>('');
+  const [value, setValue] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [animation, setAnimation] = useState<string>('walk'); //change to gif
 
- const [response, setResponse] = useState<string>("");
-const [value, setValue] = useState<string>("");
-const [loading, setLoading] = useState<boolean>(false);
-const [animation, setAnimation] = useState<string | null>(null);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setValue(e.target.value);
@@ -28,13 +29,13 @@ const [animation, setAnimation] = useState<string | null>(null);
     if (res.data.matchedCommand) {
       setAnimation(res.data.matchedCommand); // trigger animation
 
-      // reset to default mock.png after animation plays
+      // reset to default walk.png
       setTimeout(() => {
-        setAnimation(null);
+        setAnimation('walk');
       }, 3000);
 
     } else {
-      setAnimation(null); // no animation
+      setAnimation('walk'); // no animation
     }
   } catch (err) {
     console.error(err);
@@ -43,148 +44,98 @@ const [animation, setAnimation] = useState<string | null>(null);
       setLoading(false); // loading set to false after bot sends msg
     }
   };
+//     const walkAnimation = keyframes` //walking animation loop
+//     0% {
+//       background-position: center 0%;
+//     }
+//     100% {
+//       background-position: center 100%;
+//     }
+//   `;
 
    return (
-      <>
-         <Flex
+    <>
+      <Flex
+        css={{
+          gap: '$3',
+          px: '$6',
+          flexDirection: 'column',
+          alignContent: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          '@sm': {
+            flexDirection: 'row',
+            mt: '$20',
+          },
+        }}
+        justify={'center'}
+      >
+        <Box
+          css={{
+            pt: '$13',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$5',
+          }}
+        >
+          <Box css={{ maxWidth: '600px' }}>
+            <Text h1 css={{ display: 'inline' }}>
+              Dutiful{' '}
+            </Text>
+            <Text h1 css={{ display: 'inline' }} color="primary">
+              Duck
+            </Text>
+          </Box>
+
+                  <Text
+            css={{ color: '$accents8', maxWidth: '400px' }}
+            size={'$lg'}
+            span
+          >
+            Enter a command for the dutiful duck. All commands are 1 word. If you don't get it right, the AI helper will guide you. Enter them all for an achievement. Check back later for updates and more commands.
+          </Text>
+
+          <Flex css={{ gap: '$8', pt: '$4' }} wrap={'wrap'}>
+            <Input placeholder="Enter a command" size="lg" onChange={onChange} />
+            <Button onClick={handleSubmit}>Enter</Button>
+          </Flex>
+
+          <Flex
+            wrap={'wrap'}
             css={{
-               'gap': '$3',
-               'px': '$6',
-               'flexDirection': 'column',
-               'alignContent': 'center',
-               'justifyContent': 'center',
-               // 'alignItems': 'center',
-               'width': '100%',
-               '@sm': {
-                  flexDirection: 'row',
-                  mt: '$20',
-               },
+              gap: '$8',
+              py: '$7',
+              '@sm': {
+                py: '$4',
+              },
             }}
-            justify={'center'}
-         >
-            <Box
-               css={{
-                  pt: '$13',
-
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gap: '$5',
-               }}
-            >
-               <Box
-                  css={{
-                     maxWidth: '600px',
-                  }}
-               >
-                  
-                  <Text
-                     h1
-                     css={{
-                        display: 'inline',
-                     }}
-                  >
-                     Dutiful{' '}
-                  </Text>
-               
-                  <Text
-                     h1
-                     css={{
-                        display: 'inline',
-                     }}
-                     color="primary"
-                  >
-                     Duck
-                  </Text>
-               </Box>
-
-               <Text
-                  css={{
-                     color: '$accents8',
-                     maxWidth: '400px',
-                  }}
-                  size={'$lg'}
-                  span
-               >
-                  Enter a command for the dutiful duck. All commands are 1 word. If you don't get it right, the ai helper will guide you to the right answer. Enter them all for an acheivement. 
-                  Check back later for updates and more commands
-               </Text>
-
-               <Flex
-                  css={{
-                     gap: '$8',
-                     pt: '$4',
-                  }}
-                  wrap={'wrap'}
-               >
+          >
 
                   
-                  <Input placeholder="Enter a command" size="lg" onChange={onChange} />
-                  <Button onClick={handleSubmit}>Enter</Button>
-               </Flex>
-               <Flex
-                  wrap={'wrap'}
-                  css={{
-                     'gap': '$8',
-                     'py': '$7',
-                     '@sm': {
-                        py: '$4',
-                     },
-                  }}
-               >
-<div>
-   <Text
-                     css={{
-                        color: '$accents8',
-                        maxWidth: '400px',
-                     }}
-                     size={'$lg'}
-                     span
-                  >
-                     <p> {response}</p>
-                  </Text>
+                  <Text css={{ color: '$accents8', maxWidth: '400px' }} size={'$lg'} span>
+              <p>{response}</p>
+            </Text>
+          </Flex>
+        </Box>
 
-         
-         {/* {animation && (
-   <div className="sprite-container">
-      <div className={`sprite ${animation}`} />
-  </div>
-)} */}
-      </div>
+        {/* gigf Box */}
+        <Box
+          css={{
+            '& img': {
+              width: '600px',
+              height: '900px',
+              objectFit: 'contain',
+            },
+          }}
+        >
+          <img
+            src={`/images/${animation}.gif`}
+            alt={animation || 'walking duck'}
+          />
+        </Box>
+      </Flex>
 
-               </Flex>
-            </Box>
-            {/* <Box
-               css={{
-                  '& img': {
-                     width: '775px',
-                     objectFit: 'contain',
-                  },
-               }}
-            >
-               <img src="mock.png" />
-
-
-            </Box> */}
-            
-<Box
-  css={{
-    '& img': {
-      width: '775px',
-      objectFit: 'contain',
-    },
-  }}
->
-  <img 
-    src={animation ? `/images/${animation}.png` : "/images/mock.png"} 
-    alt={animation || "Default mock image"} 
-  />
-</Box>
-
-
-         </Flex>
-         <Divider
-            css={{position: 'absolute', inset: '0p', left: '0', mt: '$10'}}
-         />
-      </>
-   );
+      <Divider css={{ position: 'absolute', inset: '0p', left: '0', mt: '$10' }} />
+    </>
+  );
 };
